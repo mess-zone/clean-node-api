c89c123f7af996dce398434b737de3dff912d5fa
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-08 13:44:42 -0300

    ensure AutUseCase class Encrypter with correct values

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,8 +1,9 @@
 const { MissingParamError } = require('../../utils/errors')
 
 module.exports = class AuthUseCase {
-  constructor (loadUserByEmailRepository) {
+  constructor (loadUserByEmailRepository, encrypterSpy) {
     this.loadUserByEmailRepository = loadUserByEmailRepository
+    this.encrypterSpy = encrypterSpy
   }
 
   async auth (email, password) {
@@ -18,6 +19,8 @@ module.exports = class AuthUseCase {
     if (!user) {
       return null
     }
+
+    await this.encrypterSpy.compare(password, user.password)
     return null
   }
 }
