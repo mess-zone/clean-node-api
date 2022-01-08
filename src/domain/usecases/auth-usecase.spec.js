059b81a581314de4e4344b3f1b5001ad949fe649
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-08 16:10:31 -0300

    change authUseCase constructor to receive an object

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -49,7 +49,11 @@ const makeSut = () => {
   const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepositorySpy()
   const tokenGeneratorSpy = makeTokenGeneratorSpy()
 
-  const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy, tokenGeneratorSpy)
+  const sut = new AuthUseCase({
+    loadUserByEmailRepository: loadUserByEmailRepositorySpy,
+    encrypter: encrypterSpy,
+    tokenGenerator: tokenGeneratorSpy
+  })
 
   return {
     sut,
@@ -81,14 +85,14 @@ describe('AuthUseCase', () => {
   })
 
   test('should throw if no LoadUserByEmailRepository is provided', async () => {
-    const sut = new AuthUseCase()
+    const sut = new AuthUseCase({})
     await expect(async () => {
       await sut.auth('any_email@mail.com', 'any_password')
     }).rejects.toThrow()
   })
 
   test('should throw if LoadUserByEmailRepository has no load method', async () => {
-    const sut = new AuthUseCase({})
+    const sut = new AuthUseCase({ loadUserByEmailRepositorySpy: {} })
     await expect(async () => {
       await sut.auth('any_email@mail.com', 'any_password')
     }).rejects.toThrow()
