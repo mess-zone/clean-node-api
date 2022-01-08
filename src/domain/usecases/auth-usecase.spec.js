a7ced0c111fe79f062a8da77f3ce8d1fd9415f00
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-08 13:31:30 -0300

    remove unnecessary ifs

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,4 +1,4 @@
-const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+const { MissingParamError } = require('../../utils/errors')
 const AuthUseCase = require('./auth-usecase')
 
 const makeSut = () => {
@@ -41,14 +41,14 @@ describe('AuthUseCase', () => {
     const sut = new AuthUseCase()
     await expect(async () => {
       await sut.auth('any_email@mail.com', 'any_password')
-    }).rejects.toThrow(new MissingParamError('loadUserByEmailRepository'))
+    }).rejects.toThrow()
   })
 
   test('should throw if LoadUserByEmailRepository has no load method', async () => {
     const sut = new AuthUseCase({})
     await expect(async () => {
       await sut.auth('any_email@mail.com', 'any_password')
-    }).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+    }).rejects.toThrow()
   })
 
   test('should return null if LoadUserByEmailRepository returns null', async () => {
