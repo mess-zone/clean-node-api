88cb2e81a0693014f52d5be15a8c9fcb37196b1a
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-08 10:36:08 -0300

    move generic errors to utils layer

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -7,7 +7,7 @@ class AuthUseCase {
 }
 
 describe('AuthUseCase', () => {
-  test('shuld throw if no email is provided', async () => {
+  test('should throw if no email is provided', async () => {
     const sut = new AuthUseCase()
     const promise = sut.auth()
     expect(promise).rejects.toThrow()
