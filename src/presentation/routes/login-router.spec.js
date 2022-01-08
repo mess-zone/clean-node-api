1968dbc43fc70fb50a78d71047fc95f4f77eb0e0
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-07 22:07:31 -0300

    ensure LoginRoutes calls EmailValidator with correct email

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -18,6 +18,7 @@ const makeSut = () => {
 const makeEmailValidatorSpy = () => {
   class EmailValidatorSpy {
     isValid (email) {
+      this.email = email
       return this.isEmailValid
     }
   }
@@ -218,5 +219,16 @@ describe('Login Router', () => {
     }
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
+  })
+  test('Should calls EmailValidator with correts email', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    await sut.route(httpRequest)
+    expect(emailValidatorSpy.email).toBe(httpRequest.body.email)
   })
 })
