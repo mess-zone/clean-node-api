7d219f1b852cdbf72085f78e0403ce80277d4f5c
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-07 21:00:25 -0300

    ensure LoginRouter returns 200 when valid credentials are provided

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -7,9 +7,11 @@ const makeSut = () => {
     auth (email, password) {
       this.email = email
       this.password = password
+      return this.accessToken
     }
   }
   const authUseCaseSpy = new AuthUseCaseSpy()
+  authUseCaseSpy.accessToken = 'valid_token'
   const sut = new LoginRouter(authUseCaseSpy)
   return {
     sut,
@@ -64,7 +66,8 @@ describe('Login Router', () => {
     expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
   })
   test('Should return 401 when invalid credentials are provided', () => {
-    const { sut } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
+    authUseCaseSpy.accessToken = null
     const httpRequest = {
       body: {
         email: 'invalid_email@email.com',
@@ -74,6 +77,17 @@ describe('Login Router', () => {
     const httpResponde = sut.route(httpRequest)
     expect(httpResponde.statusCode).toBe(401)
     expect(httpResponde.body).toEqual(new UnauthorizedError())
+  })
+  test('Should return 200 when valid credentials are provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'valid_email@email.com',
+        password: 'valid_password'
+      }
+    }
+    const httpResponde = sut.route(httpRequest)
+    expect(httpResponde.statusCode).toBe(200)
   })
   test('Should return 500 if no AuthUseCase is provided', () => {
     const sut = new LoginRouter()
