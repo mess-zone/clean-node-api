7bfa5fd9ca3a0ca34820ea2e3ae0aa83e598fdf9
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-09 16:20:24 -0300

    add runInBand option to jest script to run tests sequenctially

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -1,25 +1,31 @@
 const request = require('supertest')
-const app = require('../config/app')
 
 describe('Content Type Middleware', () => {
+  let app
+
+  beforeEach(() => {
+    jest.resetModules()
+    app = require('../config/app')
+  })
+
   test('should return json content-type as default', async () => {
-    app.get('/test_content_type_default', (req, res) => {
+    app.get('/test_content_type', (req, res) => {
       res.send('')
     })
 
     await request(app)
-      .get('/test_content_type_default')
+      .get('/test_content_type')
       .expect('content-type', /json/)
   })
 
   test('should return xml content-type if forced', async () => {
-    app.get('/test_content_type_xml', (req, res) => {
+    app.get('/test_content_type', (req, res) => {
       res.type('xml')
       res.send('')
     })
 
     await request(app)
-      .get('/test_content_type_xml')
+      .get('/test_content_type')
       .expect('content-type', /xml/)
   })
 })
--- src/main/middlewares/json-parser.test.js
+++ src/main/middlewares/json-parser.test.js
@@ -3,12 +3,12 @@ const app = require('../config/app')
 
 describe('JSON Parser Middleware', () => {
   test('shoud parser body as json', async () => {
-    app.post('/test_json-parser', (req, res) => {
+    app.post('/test_json_parser', (req, res) => {
       res.send(req.body)
     })
 
     await request(app)
-      .post('/test_json-parser')
+      .post('/test_json_parser')
       .send({ any: 'any' })
       .expect({ any: 'any' })
   })
